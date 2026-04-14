dfeb2abd74d66230b1873d6bce18d1a66e52d60a
usersy628 <kimsy628@gmail.com>
2026-04-14 10:46:46 +0900

    Refactor: RequestMapping 적용

--- src/main/java/com/schedule/controller/ScheduleController.java
+++ src/main/java/com/schedule/controller/ScheduleController.java
@@ -11,26 +11,27 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping(value = "/schedules")
 public class ScheduleController {
 
     private final ScheduleService scheduleService;
 
-    @PostMapping("/schedules")
+    @PostMapping
     public ResponseEntity<CreateScheduleResponse> createSchedule(@RequestBody CreateScheduleRequest request) {
         return ResponseEntity.status(HttpStatus.CREATED).body(scheduleService.save(request));
     }
 
-    @GetMapping("/schedules/{scheduleId}")
+    @GetMapping("/{scheduleId}")
     public ResponseEntity<GetScheduleResponse> getOneSchedule(@PathVariable Long scheduleId) {
         return ResponseEntity.status(HttpStatus.OK).body(scheduleService.getOne(scheduleId));
     }
 
-    @GetMapping("/schedules")
+    @GetMapping
     public ResponseEntity<List<GetScheduleResponse>> getSchedules(@RequestParam(required = false) String userName) {
         return ResponseEntity.status(HttpStatus.OK).body(scheduleService.getAll(userName));
     }
 
-    @PatchMapping("/schedules/{scheduleId}")
+    @PatchMapping("/{scheduleId}")
     public ResponseEntity<UpdateScheduleResponse> updateSchedule(@PathVariable Long scheduleId, @RequestBody UpdateScheduleRequest request) {
         try {
             return ResponseEntity.status(HttpStatus.OK).body(scheduleService.update(scheduleId, request));
@@ -39,7 +40,7 @@ public class ScheduleController {
         }
     }
 
-    @DeleteMapping("/schedules/{scheduleId}")
+    @DeleteMapping("/{scheduleId}")
     public ResponseEntity<Void> deleteSchedule(@PathVariable Long scheduleId, @RequestParam String password) {
         try {
             scheduleService.delete(scheduleId, password);
--- src/main/java/com/schedule/repository/ScheduleRepository.java
+++ src/main/java/com/schedule/repository/ScheduleRepository.java
@@ -2,9 +2,11 @@ package com.schedule.repository;
 
 import com.schedule.entity.Schedule;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
     List<Schedule> findAllByOrderByModifiedAtDesc();
 
